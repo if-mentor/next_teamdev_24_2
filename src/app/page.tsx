1bf73b1ff5330de26163fe38681e3208ed0399b0
YasunoriMizuno <yasunorimizuno@icloud.com>
2026-04-05 20:10:58 +0900

    Header コンポーネント スタイル追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Header from "@/components/Header";
 import styles from "./styles.module.css";
 
 export default function Home() {
   return (
     <div>
+      <Header />
       <h1 className={styles.title}>Hello Teamdev!!</h1>
     </div>
   );
