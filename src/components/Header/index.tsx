cd7e04a2cf86657d4474840209aa01febc0ea7f7
YasunoriMizuno <yasunorimizuno@icloud.com>
2026-04-08 00:16:08 +0900

    Header ボタン遷移機能追加・スタイル修正

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,22 +1,30 @@
 "use client";
 
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 import styles from "./styles.module.css";
 
 const Header = () => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const router = useRouter();
   return (
     <header className={styles.header}>
       {isLoggedIn ? (
         <>
-          <button className={styles.loginButton}>新規作成</button>
+          <button onClick={() => router.push("/articles/new")} className={styles.loginButton}>
+            新規作成
+          </button>
           <button className={styles.registerButton}>ログアウト</button>
         </>
       ) : (
         <>
-          <button className={styles.loginButton}>ログイン</button>
-          <button className={styles.registerButton}>新規登録</button>
+          <button onClick={() => router.push("/login")} className={styles.loginButton}>
+            ログイン
+          </button>
+          <button onClick={() => router.push("/signup")} className={styles.registerButton}>
+            新規登録
+          </button>
         </>
       )}
     </header>
