c22c47c3f58e3237330fd62ded3fa45072fb0cdd
YasunoriMizuno <yasunorimizuno@icloud.com>
2026-04-08 23:44:36 +0900

    Header Linkコンポーネントに変更

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,30 +1,30 @@
 "use client";
 
 import { useState } from "react";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 import styles from "./styles.module.css";
 
 const Header = () => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const router = useRouter();
+
   return (
     <header className={styles.header}>
       {isLoggedIn ? (
         <>
-          <button onClick={() => router.push("/articles/new")} className={styles.loginButton}>
+          <Link href="/articles/new" className={styles.loginButton}>
             新規作成
-          </button>
+          </Link>
           <button className={styles.registerButton}>ログアウト</button>
         </>
       ) : (
         <>
-          <button onClick={() => router.push("/login")} className={styles.loginButton}>
+          <Link href="/login" className={styles.loginButton}>
             ログイン
-          </button>
-          <button onClick={() => router.push("/signup")} className={styles.registerButton}>
+          </Link>
+          <Link href="/signup" className={styles.registerButton}>
             新規登録
-          </button>
+          </Link>
         </>
       )}
     </header>
